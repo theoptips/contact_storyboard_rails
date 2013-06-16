319078331d140de7142c4cdd2483430e01b36011
Dilys Sun <optanovo@gmail.com>
2013-06-16 11:31:04 -0700

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Me2u::Application.config.secret_token = '8c55c9ff21a3c1583abd9848e62c8ca423215ff57373cbebbe56fc7f7fe991fec384c7760a3a35e3b6af7bae1d40375826a92ccbe656dc66655cb8dbf0943d8d'
